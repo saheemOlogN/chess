83c39f9e594735089bb0208c63d3fe5ec58e5d77
Saheem <saheem.nakhwa24@gmail.com>
2026-06-19 22:18:07 +0530

    ghee khaatm

---
+++ app.js
@@ -0,0 +1,22 @@
+const express= require("express")
+const socket = require("socket.io")
+const http=require("http")
+const {Chess} = require("chess.js")
+
+const app=express()
+
+const server = http.createServer(app)
+const io = socket(server)
+
+const chess=new Chess()
+
+let players = {}
+let currentPlayer = "W"
+app.set("view engine","ejs")
+app.use(express.static(path.join(__dirname,"public")))
+
+
+app.get("/",(req,res)=>{
+    res.render("index")
+
+})
