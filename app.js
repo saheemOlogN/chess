571fb5a8ec9f08dc06306d73c6b8e2899a84fe74
Saheem <saheem.nakhwa24@gmail.com>
2026-07-03 18:45:19 +0530

    ghee refill

--- app.js
+++ app.js
@@ -1,22 +1,30 @@
-const express= require("express")
+const express = require("express")
 const socket = require("socket.io")
-const http=require("http")
-const {Chess} = require("chess.js")
+const http = require("http")
+const { Chess } = require("chess.js")
+const path = require("path");
 
-const app=express()
+const app = express()
 
 const server = http.createServer(app)
 const io = socket(server)
 
-const chess=new Chess()
+const chess = new Chess()
 
 let players = {}
 let currentPlayer = "W"
-app.set("view engine","ejs")
-app.use(express.static(path.join(__dirname,"public")))
+app.set("view engine", "ejs")
+app.use(express.static(path.join(__dirname, "public")))
 
 
-app.get("/",(req,res)=>{
-    res.render("index")
+app.get("/", (req, res) => {
+    res.render("index", { title: "Chess Game" })
+})
 
+io.on("connection",function(uniquesocket){
+    console.log("connected")
+})
+
+server.listen(3000, function () {
+    console.log("ghee ghee ghee")
 })
