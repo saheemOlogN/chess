512c594afeb8e2938b522c10d6ff69e46b45b874
Saheem <saheem.nakhwa24@gmail.com>
2026-07-05 17:38:07 +0530

    updated some stuff but gotta fix more broken part

--- app.js
+++ app.js
@@ -23,6 +23,50 @@ app.get("/", (req, res) => {
 
 io.on("connection",function(uniquesocket){
     console.log("connected")
+
+    if(!players.white){
+        players.white=uniquesocket.id;
+        uniquesocket.emit("playerRole","w")
+
+    }
+    else if(!players.black){
+        players.black=uniquesocket.id;
+        uniquesocket.emit("playerRole","b")
+
+    }
+    else{
+        uniquesocket.emit("spectatorRole")
+    }
+
+    uniquesocket.on("disconnect",function(){
+        if(uniquesocket.id==players.white) delete players.white;
+        else if(uniquesocket.id==players.black) delete players.black;
+
+    })
+
+    uniquesocket.on("move",(move)=>{
+        try {
+            if(chess.turn()=='w' && uniquesocket.id!=players.white) return;
+            if(chess.turn()=='b' && uniquesocket.id!=players.black) return;
+
+           const result = chess.move(move)
+
+           if(result){
+            currentPlayer=chess.turn()
+            io.emit("move",move)
+            io.emit("boardState",chess.fen())
+
+           }
+           else{
+            console.log("Invalid move",move)
+            uniquesocket.emit("invalidMove",move)
+
+           }
+        } catch (error) {
+            console.log("Invalid move",move)
+            uniquesocket.emit("invalidMove",move)
+        }
+    })
 })
 
 server.listen(3000, function () {
--- public/js/chessgame.js
+++ public/js/chessgame.js
@@ -1,1 +1,73 @@
 const socket = io()
+const chess = new Chess()
+const boardElement = document.querySelector(".chessboard")
+
+let draggedPiece = null
+let sourceSquare = null
+let playerRole = null
+
+const renderBoard = () => {
+    const board = chess.board()
+    board.innerHTML = ""
+    board.forEach((row, rowIndex) => {
+        row.forEach((square, squareIndex) => {
+            const squareElement = document.createElement("div")
+            squareElement.classList.add("square",
+                (rowIndex + squareIndex) % 2 === 0 ? "light" : "dark"
+            )
+        })
+        squareElement.dataset.row = rowIndex
+        squareElement.dataset.square = squareIndex
+
+        if (square) {
+            const pieceElement = document.createElement("div")
+            pieceElement.classList.add("piece", square.color = 'w' ? 'white' : "black")
+            pieceElement.innerText = ""
+            pieceElement.draggable = playerRole === square.color
+
+            pieceElement.addEventListener("dragstart", (e) => {
+                if (pieceElement.draggable) {
+                    draggedPiece = pieceElement
+                    sourceSquare = { row: rowIndex, col: squareIndex }
+                    e.dataTransfer.setData("text/plain", "")
+                }
+
+            })
+            pieceElement.addEventListener("dargend", (e) => {
+                deaggedPiece = null
+                sourceSquare = null
+
+            })
+
+            squareElement.appendChild(pieceElement)
+        }
+        squareElement.addEventListener("dragover",(e)=>{
+            e.preventDefault();
+        })
+        squareElement.addEventListener("drop",(e)=>{
+            e.preventDefault()
+            if(draggedPiece){
+                const targetSource = {
+                    row: parseInt(squareElement.dataset.row),
+                    col:parseInt(squareElement.dataset.col)
+                }
+
+                handleMove(sourceSquare,targetSource)
+            }
+            
+        })
+        boardElement.appendChild(squareElement)
+
+
+    })
+
+}
+
+renderBoard()
+const handleMove = () => {
+
+}
+
+const getPieceUniCode = () => {
+
+}
